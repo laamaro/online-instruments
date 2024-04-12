46a01a6c792ae390e7ccaef29c0ec661ad81b319
Laura Amaro <lauraamaro1804@gmail.com>
2024-04-11 22:05:45 -0300

    generate instrument controller along with the 7 crud methods

---
+++ app/controllers/instruments_controller.rb
@@ -0,0 +1,54 @@
+class InstrumentsController < ApplicationController
+  before_action :set_instrument, only: %i[edit update show destroy]
+
+  def index
+    @instruments = Instrument.all
+  end
+
+  def show; end
+
+  def new
+    @instrument = Instrument.new
+  end
+
+  def edit; end
+
+  def create
+    @instrument = Instrument.new(instrument_params)
+    @instrument.user = current_user
+
+    if @instrument.save
+      redirect_to @instrument
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @instrument.update(instrument_params)
+        format.html { redirect_to @instrument }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @instrument.destroy
+
+    respond_to do |format|
+      format.html { redirect_to instruments_path }
+    end
+  end
+
+  private
+
+  def instrument_params
+    params.require(:instrument).permit(:title)
+  end
+
+  def set_instrument
+    @instrument = Instrument.find(params[:id])
+  end
+end
