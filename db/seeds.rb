743de05133877b3f0c73645d490bfb612bfbeda2
Laura Amaro <lauraamaro1804@gmail.com>
2024-04-11 21:23:01 -0300

    finish seeds for instruments questions and answers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,23 @@ User.destroy_all
 puts "criando instrumentos com suas respectivas perguntas e opções de respostas..."
 
 20.times do |i|
-  instrument = Instrument.create!(title: "Instrumento #{i}")
+  instrument = Instrument.create!(title: "Instrumento #{i + 1}")
 
   5.times do |j|
-    question = Question.create!(description: "Questão #{j}", instrument: instrument)
+    question = Question.create!(description: "Questão #{j + 1}", instrument: instrument)
 
-    ['A', 'B', 'C', 'D'].each do |option|
-      Answer.create!(description: option, question: question)
+    ['A', 'B', 'C', 'D'].each_with_index do |option, index|
+      score = case index
+              when 0 then 3
+              when 1 then 2
+              when 2 then 1
+              when 3 then 0
+              end
+
+      Answer.create!(description: option,
+                     question: question,
+                     position: index + 1,
+                     score: score)
     end
   end
 end
