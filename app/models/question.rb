fd7796b20a1f742767da99d621aca93cf319891e
Laura Amaro <lauraamaro1804@gmail.com>
2024-04-14 22:28:38 -0300

    tests and validations for question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,7 @@ class Question < ApplicationRecord
   # Associations
   belongs_to :instrument
   has_many :answers, dependent: :destroy
+
+  # Validations
+  validates_length_of :answers, maximum: 4
 end
--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class QuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @question1 = questions(:one)
+  end
+
+  test "should not save without instrument" do
+    question_test = Question.new(description: 'Teste')
+
+    assert_not question_test.valid?
+    assert @question1.valid?
+  end
 end
