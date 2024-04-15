b2e5f9037e045f7c429a6197a1695e491a95e835
Laura Amaro <lauraamaro1804@gmail.com>
2024-04-15 00:02:09 -0300

    unit tests for answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,4 @@ class Question < ApplicationRecord
   # Associations
   belongs_to :instrument
   has_many :answers, dependent: :destroy
-
-  # Validations
-  validates_length_of :answers, maximum: 4
 end
