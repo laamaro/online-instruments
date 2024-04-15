70bde1ce7353406a8f85367669133066dc15d4c2
Laura Amaro <lauraamaro1804@gmail.com>
2024-04-15 00:52:31 -0300

    unit test for applied instrument

--- test/models/applied_instrument_test.rb
+++ test/models/applied_instrument_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class AppliedInstrumentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @applied_instrument = applied_instruments(:one)
+    @patient = users(:patient)
+    @instrument = instruments(:one)
+  end
+
+  test "should not have the same instrument for user" do
+    applies_intrument_test = AppliedInstrument.new(user: @patient, instrument: @instrument, status: 0)
+
+    assert_not applies_intrument_test.valid?
+  end
 end
--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -3,12 +3,12 @@ require "test_helper"
 class QuestionTest < ActiveSupport::TestCase
   setup do
     @question1 = questions(:one)
+    @instrument = instruments(:one)
   end
 
-  test "should not save without instrument" do
-    question_test = Question.new(description: 'Teste')
+  test "should not save if instrument has already five questions" do
+    question_extra = Question.new(description: 'Teste2', instrument: @instrument)
 
-    assert_not question_test.valid?
-    assert @question1.valid?
+    assert_not question_extra.valid?, 'Max of five questions for instrument'
   end
 end
